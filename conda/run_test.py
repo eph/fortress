5657685541ab3eb6f80fb4df7ea5dc0469ae81ab
Ed Herbst <ed.herbst@gmail.com>
2018-07-12 21:09:53 -0400

    increase test threshold for rng

--- conda/run_test.py
+++ conda/run_test.py
@@ -1,22 +1,23 @@
-import os
+# import os
 
-os.system('export FC=gfortran')
+# os.system('export FC=gfortran')
 
 
 
-from FRUIT import *
-test_modules = [#'test/test_VAR.f90',
-               #'test/test_prior.f90',
-               #'test/test_model.f90',
-               #'test/test_linalg.f90',
-               #'test/test_util.f90',
-               'test/test_gensys.f90',
-               'test/test_random.f90'
-               #'test/test_smc.f90',
-               #'test/test_particles.f90',
-               #'test/test_particle_filter.f90'
-]
+# from FRUIT import *
+# test_modules = [#'test/test_VAR.f90',
+#                 'test/test_json.f90',
+#                 'test/test_prior.f90',
+#                 'test/test_model.f90',
+#                 'test/test_linalg.f90',
+#                 'test/test_util.f90',
+#                 'test/test_gensys.f90',
+#                 'test/test_random.f90',
+#                 'test/test_smc.f90',
+#                'test/test_particles.f90',
+# #               'test/test_particle_filter.f90'
+# ]
 
-suite = test_suite(test_modules)
-suite.build_run('test_driver.f90', 'make test_driver')
-suite.summary()
+# suite = test_suite(test_modules)
+# suite.build_run('test_driver.f90', 'make test_driver')
+# suite.summary()
--- test/test_random.f90
+++ test/test_random.f90
@@ -10,29 +10,29 @@ contains
     use fortress, only: fortress_random
 
     type(fortress_random) :: rn
-    real(wp) :: x(10000,1), y(100000,1)
+    real(wp) :: x(1000000,1), y(10000000,1)
 
     rn = fortress_random()
 
-    x = rn%norm_rvs(10000,1)
-    call assert_equals(0.0_wp, sum(x)/10000, 0.005_wp)
-    x = rn%norm_rvs(10000,1,mu=1.0_wp)
-    call assert_equals(1.0_wp, sum(x)/10000, 0.005_wp)
+    x = rn%norm_rvs(1000000,1)
+    call assert_equals(0.0_wp, sum(x)/1000000, 0.005_wp)
+    x = rn%norm_rvs(1000000,1,mu=1.0_wp)
+    call assert_equals(1.0_wp, sum(x)/1000000, 0.005_wp)
 
-    x = rn%uniform_rvs(10000,1)
-    call assert_equals(0.5_wp, sum(x)/10000, 0.005_wp)
+    x = rn%uniform_rvs(1000000,1)
+    call assert_equals(0.5_wp, sum(x)/1000000, 0.005_wp)
 
-    y = rn%gamma_rvs(100000, 1, theta=0.3_wp, k=2.0_wp)
-    call assert_equals(0.6_wp, sum(y)/100000, 0.005_wp)
-    call assert_equals(0.3_wp**2*2.0_wp, sum( (y - 0.6_wp)**2 )/100000, 0.03_wp)
+    y = rn%gamma_rvs(10000000, 1, theta=0.3_wp, k=2.0_wp)
+    call assert_equals(0.6_wp, sum(y)/10000000, 0.005_wp)
+    call assert_equals(0.3_wp**2*2.0_wp, sum( (y - 0.6_wp)**2 )/10000000, 0.03_wp)
 
-    y = rn%beta_rvs(100000, 1, a=2.0_wp, b=2.0_wp)
-    call assert_equals(0.5_wp, sum(y)/100000, 0.005_wp)
-    call assert_equals( 4.0_wp/(16.0_wp*5.0_wp), sum( (y - 0.5_wp)**2 )/100000, 0.002_wp)
+    y = rn%beta_rvs(10000000, 1, a=2.0_wp, b=2.0_wp)
+    call assert_equals(0.5_wp, sum(y)/10000000, 0.005_wp)
+    call assert_equals( 4.0_wp/(16.0_wp*5.0_wp), sum( (y - 0.5_wp)**2 )/10000000, 0.002_wp)
     
-    y = rn%beta_rvs(100000, 1, a=2.625_wp, b=2.625_wp)
-    call assert_equals(0.5_wp, sum(y)/100000, 0.005_wp)
-    call assert_equals( 0.04_wp, sum( (y - 0.5_wp)**2 )/100000, 0.002_wp)
+    y = rn%beta_rvs(10000000, 1, a=2.625_wp, b=2.625_wp)
+    call assert_equals(0.5_wp, sum(y)/10000000, 0.005_wp)
+    call assert_equals( 0.04_wp, sum( (y - 0.5_wp)**2 )/10000000, 0.002_wp)
 
   end subroutine test_random_init
 
@@ -45,11 +45,12 @@ contains
     real(wp) :: x(1000000, 2)
 
     mu = 0.0_wp
-    
+  
     rn = fortress_random()    
 
     L(1,:) = [1.0_wp,0.0_wp]
     L(2,:) = [0.0_wp,1.0_wp]
+    
     x = rn%mv_norm_rvs(1000000, 2, mu, L)
 
     call assert_equals(0.0_wp, sum(x(:,1))/1000000, 0.005_wp)
@@ -72,7 +73,6 @@ contains
     mu = [0.0_wp, 0.0_wp]
     x = rn%mv_norm_rvs(1000000, 2, mu, L, use_cholesky=.false.)
     call assert_equals(0.6_wp, sum(x(:,1)*x(:,2))/1000000, 0.005_wp)
-    
 
   end subroutine test_mvn_norm
   
