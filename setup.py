689f9fc499a82cb054169b6520685949194601a4
Ed Herbst <ed.herbst@gmail.com>
2021-03-11 16:17:45 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import numpy.distutils.core
 if __name__ == "__main__":
     numpy.distutils.core.setup(
         name='fortress',
-        version='0.1.3',
+        version='0.1.5',
         platforms='linux',
         packages=['fortress'])
